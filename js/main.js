0d000102ac40298fb60050e52b978947e051f61b
chadbanghart <cbanghar@gmail.com>
2024-03-28 17:05:15 -0700

    define required constants. update init variables to reflected variables

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
   /*----- constants -----*/
-
-
+  const SUITS = ['s', 'c', 'd', 'h'];
+  const RANKS = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+  const ORIGINAL_DECK = buildOriginalDeck();
+  const MINIMUM_BET = 10;
+  const INITIAL_PLAYER_BANK = 100;
+  const ODDS_PAYOUT = 3/2;
   /*----- state variables -----*/
   
   let handStatus;
-  let deck;
+  let shuffledDeck;
   let playerHand;
   let dealerHand;
   let betAmount;
@@ -22,14 +26,18 @@ init();
 
   function init() {
     handStatus = null;
-    deck = [];
+    shuffledDeck = [];
     playerHand = [];
     dealerHand = [];
-    betAmount = 10;
-    playerBank = 100;
+    betAmount = MINIMUM_BET;
+    playerBank = INITIAL_PLAYER_BANK;
     render();
   }
 
   function render() {
+
+  }
+
+  function buildOriginalDeck() {
 
   }
